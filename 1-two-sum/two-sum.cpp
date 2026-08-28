e1992c73b802a086ea87c52a4ec3d483b0dd048d
jogender jangir <jogenderxg@gmail.com>
2026-08-28 22:43:45 +0530

    Time: 142 ms (5.06%) | Memory: 14.2 MB (63.52%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+        vector<int> res;
+
+        for(int i = 0; i < nums.size(); i++) {
+
+            for(int j = 0; j < nums.size(); j++) {
+
+                if(i != j && nums[i] + nums[j] == target) {
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
+                }
+            }
+        }
+
+        return res;
+    }
+};
