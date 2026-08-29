abc58d9d708f06d204a0c68f59b0b1497963a9a7
jogender jangir <jogenderxg@gmail.com>
2026-08-29 17:19:32 +0530

    Time: 2 ms (72.61%) | Memory: 16 MB (5.68%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
-        vector<int> res;
-
-        for(int i = 0; i < nums.size(); i++) {
-
-            for(int j = 0; j < nums.size(); j++) {
-
-                if(i != j && nums[i] + nums[j] == target) {
-                    res.push_back(i);
-                    res.push_back(j);
-                    return res;
-                }
+            unordered_map<int,int> map;
+            vector<int> res;
+            for(int i=0;i<nums.size();i++){
+               map.insert({nums[i], i}); 
             }
-        }
+            for(int i=0;i<nums.size();i++){
+                int lookup = target-nums[i];
+                if(map.find(lookup) != map.end() && map[lookup]!=i){
 
-        return res;
-    }
+                         res.push_back(i);
+                res.push_back(map[lookup]);
+                return res;
+                }  //if the lookup elemt exist 
+                
+            }       
+            return res;
+
+    };
 };
+//  for(int i = 0; i < nums.size(); i++) {
+
+        //     for(int j = 0; j < nums.size(); j++) {
+
+        //         if(i != j && nums[i] + nums[j] == target) {
+        //             res.push_back(i);
+        //             res.push_back(j);
+        //             return res;
+        //         }
+        //     }
+        // }
+
+        // return res;
+        // for(int i = 0; i < nums.size(); i++) {
+
+        //     for(int j = 0; j < nums.size(); j++) {
+
+        //         if(i != j && nums[i] + nums[j] == target) {
+        //             res.push_back(i);
+        //             res.push_back(j);
+        //             return res;
+        //         }
+        //     }
+        // }
+
+        // return res;
