41d886159c7620e950a32d7c35dd48be08f38b59
jogender jangir <jogenderxg@gmail.com>
2026-08-31 20:41:06 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (80.68%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -6,7 +6,9 @@ public:
             if(nums[j] != nums[j-1]){
                 nums[k]=nums[j];
                 k++;
-
+                     // tho tho final nums value will be 
+                     //for test case  [0,0,1,1,1,2,2,3,3,4]
+                     // 0,1,2,3,4,2,2,3,3,4 
             } 
             
         }
